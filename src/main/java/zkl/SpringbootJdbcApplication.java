4fb567c166ec8c8fc7180c1c22dc13aed9281163
a1468151338 <a1468151338@outlook.com>
2018-01-30 09:08:26 +0800

    main

--- src/main/java/zkl/SpringbootJdbcApplication.java
+++ src/main/java/zkl/SpringbootJdbcApplication.java
@@ -1,16 +1,7 @@
 package zkl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.MultipartConfigElement;
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SpringbootJdbcApplication {
